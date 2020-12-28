32898d23451293b607472684dc2d6f2ab183ff01
Khamza <khamza@inbox.ru>
2020-12-28 20:27:42 +0500

    init commit

---
+++ src/home/Controller.java
@@ -0,0 +1,168 @@
+package home;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private VBox pnItems = null;
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnOrders;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnMenus;
+
+    @FXML
+    private Button btnPackages;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    private Pane pnlCustomer;
+
+    @FXML
+    private Pane pnlOrders;
+
+    @FXML
+    private Pane pnlOverview;
+
+    @FXML
+    private Pane pnlMenus;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Label lblTimer;
+
+    @FXML
+    private Label lblNotReceived;
+
+    private int time = 10;
+
+    public static Connection connection = ConnectionDB.getConnection("online_ordering");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(1.0), e -> {
+                    refreshData();
+                })
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
+
+        Node[] nodes = new Node[10];
+        for (int i = 0; i < nodes.length; i++) {
+            try {
+
+                final int j = i;
+                nodes[i] = FXMLLoader.load(getClass().getResource("Item.fxml"));
+
+                //give the items some effect
+
+                nodes[i].setOnMouseEntered(event -> {
+                    nodes[j].setStyle("-fx-background-color : #0A0E3F");
+                });
+                nodes[i].setOnMouseExited(event -> {
+                    nodes[j].setStyle("-fx-background-color : #02030A");
+                });
+                pnItems.getChildren().add(nodes[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void refreshData() {
+        lblTimer.setText(String.valueOf(time--));
+        if (time == -1) {
+
+            // connect db
+
+            String SELECT_ORDER_PIZZA = "SELECT * FROM order_pizza";
+
+            try {
+                Connection connection = ConnectionDB.getConnection("online_ordering");
+                PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ORDER_PIZZA);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                int count = 0;
+                while (resultSet.next()) {
+                    count++;
+                }
+                lblNotReceived.setText(count + "");
+
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+
+            time = 10;
+        }
+    }
+
+
+    public void handleClicks(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == btnCustomers) {
+            pnlCustomer.setStyle("-fx-background-color : #1620A1");
+            pnlCustomer.toFront();
+        }
+        if (actionEvent.getSource() == btnMenus) {
+            pnlMenus.setStyle("-fx-background-color : #53639F");
+            pnlMenus.toFront();
+        }
+        if (actionEvent.getSource() == btnOverview) {
+            pnlOverview.setStyle("-fx-background-color : #02030A");
+            pnlOverview.toFront();
+        }
+        if (actionEvent.getSource() == btnOrders) {
+            pnlOrders.setStyle("-fx-background-color : #464F67");
+            pnlOrders.toFront();
+        }
+
+        if (actionEvent.getSource() == btnExit) {
+            Stage stage = (Stage) btnExit.getScene().getWindow();
+            stage.close();
+        }
+    }
+}
+
